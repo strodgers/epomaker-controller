84554361a17a3e2679cb2946cfc74e47a3d4e08e
Sam Rodgers <samueltrodgers@duck.com>
2024-09-23 15:41:46 +0100

    Docker image only creates udev rules if it needs to, then just passes args to epomaker-controller within container

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-# Apply epomaker udev rules
-epomakercontroller dev --udev
+# Apply epomaker udev rules if necessary
+if [ ! -f /etc/udev/rules.d/99-epomaker-rt100.rules ]; then
+    epomakercontroller dev --udev >/dev/null 2>&1
+fi
 
-# Switch to non-root user to run the application
-exec su - nonrootuser -s /bin/bash
+APP_ARGS="$@"
+
+# Switch to non-root user and execute the application with the passed arguments
+exec su - nonrootuser -c "epomakercontroller $APP_ARGS"
